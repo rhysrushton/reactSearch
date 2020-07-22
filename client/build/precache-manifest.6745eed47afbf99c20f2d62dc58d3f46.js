3f11e333a768c91e327e833e61fab5d626b7284f
Rhys Rushton <rhys.rushton@gmail.com>
2020-07-22 10:33:19 +1000

    change

---
+++ client/build/precache-manifest.6745eed47afbf99c20f2d62dc58d3f46.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "8f730eb99e8521f60518",
+    "url": "/static/js/main.d448626a.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "f20b337d9e3686959504",
+    "url": "/static/js/2.d49b467c.chunk.js"
+  },
+  {
+    "revision": "6cc90b034b4a37c06643e4c19fa2e1c6",
+    "url": "/index.html"
+  }
+];
